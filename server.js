60685266be2b57509a52f5847f48b693da9eda79
rmahawewa <rmahawewa@yahoo.com>
2025-05-09 11:42:19 +0530

    make routes with express

---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var url = require('url');
+
+
+const server = http.createServer((req, res) => {
+    
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    res.end();
+})
